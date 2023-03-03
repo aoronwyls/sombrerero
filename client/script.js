e958d766dbe9c49aff739ec2fa11e062ca8690ab
Alan OR <98626161+aoronwyls@users.noreply.github.com>
2023-03-02 21:38:42 -0500

    actualizacion url server

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://yarbees.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
